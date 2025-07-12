07c7339f3b6fc3d42a1fea9f0bac9e06835015e7
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2025-07-13 00:40:20 +0530

    Add main Spring Boot application class
    Signed-off-by: Sahil Jaiswal <jaiswalsahil7741@gmail.com>

---
+++ backend/src/main/java/com/cloudsketch/WhiteboardApplication.java
@@ -0,0 +1,14 @@
+package com.cloudsketch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhiteboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhiteboardApplication.class, args);
+        System.out.println("CloudSketch Backend Started!");
+    }
+
+}

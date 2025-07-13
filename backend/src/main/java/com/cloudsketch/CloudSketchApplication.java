e6271fc84561ebf64001dbca7df0806b379e0095
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2025-07-13 10:42:25 +0530

    Update backend with complete dependencies and database config
    Signed-off-by: Sahil Jaiswal <jaiswalsahil7741@gmail.com>

--- backend/src/main/java/com/cloudsketch/CloudSketchApplication.java
+++ backend/src/main/java/com/cloudsketch/CloudSketchApplication.java
@@ -3,6 +3,10 @@ package com.cloudsketch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main application class for CloudSketch Whiteboard Backend
+ * This class bootstraps the Spring Boot application with authentication and WebSocket support
+ */
 @SpringBootApplication
 public class CloudSketchApplication {
 

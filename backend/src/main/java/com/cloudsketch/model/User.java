60c8073f1a009516d5fdbea188da7d925a1d1c71
Sahil Jaiswal <jaiswalsahil7741@gmail.com>
2025-07-13 10:43:00 +0530

    Add JPA entity User model and application properties
    Signed-off-by: Sahil Jaiswal <jaiswalsahil7741@gmail.com>

--- backend/src/main/java/com/cloudsketch/model/User.java
+++ backend/src/main/java/com/cloudsketch/model/User.java
@@ -1,33 +1,61 @@
 package com.cloudsketch.model;
 
-public class User {
-    private String id;
-    private String username;
-    private String email;
-    private String currentRoom;
-    private long lastActive;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
+/**
+ * User entity representing registered users in the system
+ * Stores user credentials with encrypted passwords
+ */
+@Entity
+@Table(name = "users", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "username")
+})
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Size(max = 50)
+    @Column(unique = true)
+    private String username;
+
+    @NotBlank
+    @Size(max = 100)
+    private String password;
+
+    // Constructors
     public User() {}
 
-    public User(String id, String username, String email) {
-        this.id = id;
+    public User(String username, String password) {
         this.username = username;
-        this.email = email;
-        this.lastActive = System.currentTimeMillis();
+        this.password = password;
     }
 
-    public String getId() { return id; }
-    public void setId(String id) { this.id = id; }
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
 
-    public String getUsername() { return username; }
-    public void setUsername(String username) { this.username = username; }
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-    public String getEmail() { return email; }
-    public void setEmail(String email) { this.email = email; }
+    public String getUsername() {
+        return username;
+    }
 
-    public String getCurrentRoom() { return currentRoom; }
-    public void setCurrentRoom(String currentRoom) { this.currentRoom = currentRoom; }
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    public long getLastActive() { return lastActive; }
-    public void setLastActive(long lastActive) { this.lastActive = lastActive; }
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
